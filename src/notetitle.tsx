335a4c5ae2284d12bb067946e166ba0c6d72c3b0
Jordon Wing <jordonwii@gmail.com>
2019-09-22 18:05:15 -0700

    Simple contentEditable title.

--- src/notetitle.tsx
+++ src/notetitle.tsx
@@ -7,20 +7,34 @@ interface NoteTitleProps {
 
 interface NoteTitleState {
     title: string
+    editing: boolean;
 }
 
 export class NoteTitle extends React.Component<NoteTitleProps, NoteTitleState> {
+    divRef = React.createRef<HTMLDivElement>();
+
     constructor(props: NoteTitleProps) {
         super(props);
 
-        this.state = { title: this.props.initialTitle }
+        this.state = { title: this.props.initialTitle, editing: false }
     }
 
     render() {
         return (
-            <div className="note-title">
+            <div className="note-title" ref={this.divRef} onClick={this.handleClick.bind(this)} contentEditable={true} onKeyDown={this.handleKeyDown.bind(this)}>
                 {this.state.title}
             </div>
         )
+    }
+
+    handleClick(e: React.MouseEvent) {
+        this.setState({ editing: true });
+    }
+
+    handleKeyDown(e: React.KeyboardEvent) {
+        if (e.key=== 'Enter' && this.divRef.current !== null) {
+            e.preventDefault();
+            this.setState({ title: this.divRef.current.textContent || "", editing: false})
+        }
     }
 }
