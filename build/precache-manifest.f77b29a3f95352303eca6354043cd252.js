021fb0db1ba5367d528765246612b436ad0eafb9
Jordon Wing <jordonwii@gmail.com>
2019-12-05 23:05:43 -0800

    Commit build v1

---
+++ build/precache-manifest.f77b29a3f95352303eca6354043cd252.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7604a8e8205ad4affb46102ac6686ac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "79eb9fecbac441d40b44",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "f2f8a0ed5063d835f80c",
+    "url": "/static/css/main.6fcba6c7.chunk.css"
+  },
+  {
+    "revision": "79eb9fecbac441d40b44",
+    "url": "/static/js/2.0b871bfe.chunk.js"
+  },
+  {
+    "revision": "f2f8a0ed5063d835f80c",
+    "url": "/static/js/main.c1cae3aa.chunk.js"
+  },
+  {
+    "revision": "625d242778931064f47d",
+    "url": "/static/js/runtime-main.cf387e41.js"
+  }
+]);
