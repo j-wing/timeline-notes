bab83b430100d55d97d8d8fb7a45b3e3ff0fde97
Jordon Wing <jordonwii@gmail.com>
2020-02-29 21:41:43 -0800

    Updates

---
+++ precache-manifest.338e4aa1a6b162a017a54886aeef7f52.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6960a577f068461ce6af437cbabc2609",
+    "url": "/index.html"
+  },
+  {
+    "revision": "26c2d3fa7942cfadfc55",
+    "url": "/static/css/2.2ec3db0d.chunk.css"
+  },
+  {
+    "revision": "23ec9190bedd9089f748",
+    "url": "/static/css/main.e705e627.chunk.css"
+  },
+  {
+    "revision": "26c2d3fa7942cfadfc55",
+    "url": "/static/js/2.58827893.chunk.js"
+  },
+  {
+    "revision": "0044596a561c04a6a036dc3bf4809ed7",
+    "url": "/static/js/2.58827893.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23ec9190bedd9089f748",
+    "url": "/static/js/main.4859d67f.chunk.js"
+  },
+  {
+    "revision": "2b1a6c8861028632d796",
+    "url": "/static/js/runtime-main.6f4b2e18.js"
+  }
+]);
